82f88b763d008102920cab40ec8232cc01b5b211
Erik Warringer <erikwarringer@gmail.com>
2017-02-17 23:28:35 -0500

    search params/routes resolve

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,5 +2,10 @@ class JobsController < ApplicationController
   # layout 'application'
   def index
     @api = IndeedAPI
+    if params[:search]
+      binding.pry
+    else
+      binding.pry
+    end
   end
 end
